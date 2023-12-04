5eddf972e6c3b06d345a641889bbe18ef5915a29
LEONARDOSAUL2720 <avilasanchezleonardosaul2@gmail.com>
2023-12-03 19:59:17 -0600

    listoo

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,27 @@
+// database.js
 import mysql from "promise-mysql";
 import config from "./../config";
 
-const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password
-});
+let connection;
+
+const connectToDatabase = async () => {
+    try {
+        connection = await mysql.createConnection({
+            host: config.host,
+            database: config.database,
+            user: config.user,
+            password: config.password
+        });
+
+        console.log("Conexion exitosa!");
+    } catch (error) {
+        console.error("Error conexion de database:", error.message);
+        throw error;
+    }
+};
 
 const getConnection = () => {
     return connection;
 };
 
-module.exports = {
-    getConnection
-};
+export { connectToDatabase, getConnection };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import app from "./app";
 
-const main = () => {
-    app.listen(app.get("port"));
-    console.log(`Server on port ${app.get("port")}`);
+import app from "./app";
+import { connectToDatabase } from "./database/database";
+
+
+const main = async () => {
+    try {
+        await connectToDatabase();
+        
+        app.listen(app.get("port"));
+        console.log(`Server on port ${app.get("port")}`);
+    } catch (error) {
+        console.error("Unable to start the server:", error.message);
+    }
 };
 
 main();
